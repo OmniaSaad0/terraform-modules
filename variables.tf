8478e6cfee2b542a8a928f5ccbd1ad0e512698a8
OmniaSaad0 <saadomnia509@gmail.com>
2025-10-01 17:37:11 +0300

    modify the modules

--- variables.tf
+++ variables.tf
@@ -4,18 +4,6 @@ variable "region" {
   default     = "us-west-1"
 }
 
-<<<<<<< Updated upstream
-variable "wordpress_ami" {
-  description = "AMI ID for WordPress instances"
-  type        = string
-  default     = "ami-0424788ce1ae8d5eb"
-}
-
-variable "mysql_ami" {
-  description = "AMI ID for MySQL instance"
-  type        = string
-  default     = "ami-0ddac4b9aed8d5d46"
-=======
 variable "name_prefix" {
   description = "Prefix for resource names"
   type        = string
@@ -63,5 +51,4 @@ variable "cpu_scale_in_threshold" {
   description = "CPU threshold for scaling in"
   type        = number
   default     = 20
->>>>>>> Stashed changes
 }
